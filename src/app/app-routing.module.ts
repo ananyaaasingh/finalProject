f296e6ae47b062a93eee7dfeffb8090cef4fa7f8
ananya singh <btbtl18062_ananya@gmail.com>
2022-04-08 09:53:03 +0530

    initial commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,65 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { DashboardComponent } from './admin/dashboard/dashboard.component';
+import { HomeComponent } from './admin/home/home.component';
+import { ProfileComponent } from './admin/profile/profile.component';
+import { QuestComponent } from './admin/quest/quest.component';
+import { QuizComponent } from './admin/quiz/quiz.component';
+import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './register/register.component';
+import { UserDashboardComponent } from './user/user-dashboard/user-dashboard.component';
+import { UserHomeComponent } from './user/user-home/user-home.component';
+import { UserProfileComponent } from './user/user-profile/user-profile.component';
+import { UserQuizComponent } from './user/user-quiz/user-quiz.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path : 'login' , component : LoginComponent
+  },
+  {
+    path : 'register' , component : RegisterComponent
+  },
+  {
+    path : 'admin-dashboard' , component : DashboardComponent,
+    children : [
+      {
+        path : 'admin-home' , component : HomeComponent
+      },
+      {
+        path : 'admin-profile' , component : ProfileComponent
+      },
+      {
+        path : 'admin-quest' , component : QuestComponent
+      },
+      {
+        path : 'admin-quiz' , component : QuizComponent
+      },
+      {
+        path : '' , redirectTo : '/admin/home' , pathMatch : 'full'
+      }
+    ]
+  },
+  {
+    path : 'user-dashboard' , component : UserDashboardComponent,
+    children : [
+      {
+        path : 'user-home' , component : UserHomeComponent
+      },
+      {
+        path : 'user-profile' , component : UserProfileComponent
+      },
+      {
+        path : 'user-quiz' , component : UserQuizComponent
+      },
+      { 
+        path : '' , redirectTo : '/user-dashboard/user-home' , pathMatch : 'full'
+      }
+    ]
+  },
+  {
+    path : '' , redirectTo : '/register' , pathMatch : 'full'
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,40 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { RegisterComponent } from './register/register.component';
+import { LoginComponent } from './login/login.component';
+import { DashboardComponent } from './admin/dashboard/dashboard.component';
+import { HeaderComponent } from './admin/header/header.component';
+import { FooterComponent } from './admin/footer/footer.component';
+import { HomeComponent } from './admin/home/home.component';
+import { ProfileComponent } from './admin/profile/profile.component';
+import { QuizComponent } from './admin/quiz/quiz.component';
+import { QuestComponent } from './admin/quest/quest.component';
+import { UserDashboardComponent } from './user/user-dashboard/user-dashboard.component';
+import { UserHeaderComponent } from './user/user-header/user-header.component';
+import { UserFooterComponent } from './user/user-footer/user-footer.component';
+import { UserHomeComponent } from './user/user-home/user-home.component';
+import { UserProfileComponent } from './user/user-profile/user-profile.component';
+import { UserQuizComponent } from './user/user-quiz/user-quiz.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    RegisterComponent,
+    LoginComponent,
+    DashboardComponent,
+    HeaderComponent,
+    FooterComponent,
+    HomeComponent,
+    ProfileComponent,
+    QuizComponent,
+    QuestComponent,
+    UserDashboardComponent,
+    UserHeaderComponent,
+    UserFooterComponent,
+    UserHomeComponent,
+    UserProfileComponent,
+    UserQuizComponent
   ],
   imports: [
     BrowserModule,
